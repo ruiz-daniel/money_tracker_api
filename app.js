154583479f1617b6c41f3acbd3de5fbd6885809e
DRGWeiss <ruizd1710@gmail.com>
2023-03-28 16:15:53 -0400

    initial app and dependencies

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+require('dotenv').config({ path: './.env' })
+const cors = require('cors')
+const app = express();
+const PORT = 3002;
+  
+app.listen(PORT, (error) =>{
+    if(!error)
+        console.log("Server is Successfully Running, and App is listening on port "+ PORT)
+    else 
+        console.log("Error occurred, server can't start", error);
+    }
+);
+
+app.use(cors())
